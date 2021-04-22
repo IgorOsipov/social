f4fc5b896aa8364485e36a2b9ff7b1efc7a03b85
Ihor Osypov <ihor.osypov@geeksforless.com>
2021-04-22 19:13:58 +0300

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import { Container } from 'react-bootstrap';
+import React from 'react';
+import { Container } from "react-bootstrap";
+import Header from "./Components/Header"
+
 
 const App = () => {
   return (
-    <div className="App">
+    <>
+      <Header />
       <Container>
-        
+        <div style={{height: "100vh", border: "1px solid gray", backgroundColor: "#eee"}}></div>
       </Container>
-    </div>
+    </>
   );
 }
 
---
+++ src/Components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Button, Form, FormControl, Nav, Navbar } from 'react-bootstrap';
+import logo from '../Img/logo.png';
+
+const Header = () => {
+    return (
+        <Navbar sticky="top" bg="dark" variant="dark" expand="lg">
+            <Navbar.Brand href="#home">
+                <img
+                    src={logo}
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                    alt="React Bootstrap logo"
+                />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto justify-content-center" defaultActiveKey="/home">
+                    <Nav.Link href="/home">Profile</Nav.Link>
+                    <Nav.Link href="/messages">Messages</Nav.Link>
+                    <Nav.Link href="/news">News</Nav.Link>
+                    <Nav.Link href="/music">Music</Nav.Link>
+                    <Nav.Link href="/settings">Settings</Nav.Link>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-success">Search</Button>
+                </Form>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default Header;
