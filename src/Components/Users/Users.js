99273cd4c4f6c899d872e8052ea96b0a0aad1bf4
Ihor Osypov <ihor.osypov@geeksforless.com>
2021-06-09 16:52:01 +0300

    Users cards style update

--- src/Components/Users/Users.js
+++ src/Components/Users/Users.js
@@ -1,30 +1,72 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
+import styled from 'styled-components';
 
+const CardStyle = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+    padding: 3%;
+
+    .card{
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+        padding: 3%;
+        margin-bottom: 0;
+
+        .cardInfo{
+            height: 300px;
+
+            
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+
+            img{
+                height: 75%;
+                width: auto;
+            }
+            button{
+                height: 15%;
+                width: 100%;
+            }
+        }
+
+        .card-body{
+            padding: 0;
+            padding-left: 5%;
+        }
+        
+    }
+`
 
 
 const Users = (props) => {
     return (
-        <div style={{ display: 'flex', justifyContent: 'flex-start', flexWrap: 'wrap' }}>
+        <CardStyle>
             {
-                props.users.map(u => {
+                props.users.map((u) => {
                     return (
-                        <Card key={u.id} style={{ width: '25%'}}>
-                            <Card.Img style={{ width: '100%', height: 'auto' }} variant="top" src="https://cdn.worldvectorlogo.com/logos/fdf.svg" />
+                        <Card key={u.id} >
+                            <div className="cardInfo">
+                                <Card.Img variant="top" src="https://cdn.worldvectorlogo.com/logos/fdf.svg" />
+                                {u.followed
+                                    ? <Button onClick={() => { props.unfollow(u.id) }} variant="primary">Unfollow</Button>
+                                    : <Button onClick={() => { props.follow(u.id) }} variant="primary">Follow</Button>
+                                }
+                            </div>
+
                             <Card.Body>
                                 <Card.Title>{u.name}</Card.Title>
                                 <Card.Text>{u.status}</Card.Text>
-                                <Card.Text>{u.location.city + ", " + u.location.country }</Card.Text>
+                                <Card.Text>{u.location.city + ", " + u.location.country}</Card.Text>
                             </Card.Body>
-                            {u.followed 
-                                ? <Button onClick={() => {props.unfollow(u.id)}} variant="primary">Unfollow</Button> 
-                                : <Button onClick={() => {props.follow(u.id)}} variant="primary">Follow</Button>
-                            }
                         </Card>
                     )
                 })
             }
-        </div>
+        </CardStyle>
     )
 }
 
