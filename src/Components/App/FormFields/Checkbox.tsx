2123cf951ee44780b5641bd80aaeaf65744f927c
Ihor Osypov <ihor.osypov@geeksforless.com>
2021-11-29 11:29:55 +0200

    typized props in chebox component

--- src/Components/App/FormFields/Checkbox.tsx
+++ src/Components/App/FormFields/Checkbox.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
+import { WrappedFieldInputProps } from 'redux-form';
 
-export const Checkbox:React.FC<{input: any}> = ({ input }) => {
+export const Checkbox:React.FC<{input: WrappedFieldInputProps}> = ({ input }) => {
     return (
         <Form.Group>
             <Form.Check {...input} id={input.name} name={input.name} label="Remember me"/>
