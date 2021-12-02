2b91ca22a37b04f492df1ea4571d95ca1c46812f
Ihor Osypov <ihor.osypov@geeksforless.com>
2021-12-02 15:31:17 +0200

    typization of HOCs

--- src/Components/HOC/WithAuthRedirect.tsx
+++ src/Components/HOC/WithAuthRedirect.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Redirect } from 'react-router-dom';
+import { AppStateType } from "../../Redux/store";
 
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state: AppStateType) => ({
     isAuth: state.auth.isAuth
 })
 
-export const withAuthRedirect = (Component) => {
-    class RedirectComponent extends React.Component{
+export const withAuthRedirect = (Component: React.ComponentType) => {
+    class RedirectComponent extends React.Component<{isAuth: boolean}>{
         render(){
             if(!this.props.isAuth) return <Redirect to='/login' />
             return <Component {...this.props} />
