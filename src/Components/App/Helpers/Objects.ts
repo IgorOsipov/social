1c7fe049faa415d6520f19bfd52f3edd2ff53daa
Ihor Osypov <ihor.osypov@geeksforless.com>
2021-12-03 19:12:06 +0200

    typized lefted components

---
+++ src/Components/App/Helpers/Objects.ts
@@ -0,0 +1,10 @@
+import { usersType } from "../../../Types/types"
+
+export const updateObjectInArray = (items: Array<usersType>, itemId: number, objPropName: string, newObjProps: {}) => {
+    return items.map(u => {
+        if (u[objPropName as keyof usersType] === itemId) {
+            return { ...u, ...newObjProps }
+        }
+        return u
+    })
+}
